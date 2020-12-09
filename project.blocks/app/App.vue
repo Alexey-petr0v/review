b00707db0b7bc783041b31129166a38b303d97ac
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-12-09 15:26:45 +0700

    Added 'button' and 'rate' blocks to 'app'; added method 'openRewiew' to show/hide reviews

--- project.blocks/app/App.vue
+++ project.blocks/app/App.vue
@@ -7,8 +7,18 @@
   import Comment from '../comment/Comment.vue';
 
   export default {
+    data: function () {
+      return {
+        hide: ""
+      }
+    },
     components: {
       Comment
+    },
+    methods: {
+      openRewiew() {
+        this.hide == "app__rewiew_hide" ? this.hide = "" : this.hide = "app__rewiew_hide"
+      }
     }
   }
 </script>
