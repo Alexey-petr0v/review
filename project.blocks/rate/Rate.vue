8b5fb5889d9cf3cafec453fedda3f2048f2eb52a
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-12-09 20:13:02 +0700

    Fixed 'rate' block: changed styles and markup, created vue component

---
+++ project.blocks/rate/Rate.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  include rate
+  +addRate
+</template>
+
+<script>
+  export default {
+    props: {
+      active: String,
+      lengthRate: {
+        type: Number,
+        default: 5
+      },
+      currentRate: {
+        type: Number,
+        default: 0
+      }
+    },
+    data: function() {
+      return {
+        active: "aaaabbb",
+        stars: new Array()
+      }
+    },
+    computed: {
+    },
+    methods: {
+      addCheck(event){
+        for (let i = 0; i < this.lengthRate; i++) {
+          this.stars[i].act = ""
+        }
+        this.currentRate = event.target.getAttribute("data-value");
+        this.addActivity(this.currentRate)
+      },
+      addActivity(currentValue) {
+        console.log(currentValue)
+        for (let i = 0; i < currentValue; i++) {
+          this.stars[i].act = "rate__title_active"
+        }
+      }
+    },
+    mounted: function () {
+      for (let i = 0; i < this.$props.lengthRate; i++) {
+        this.$data.stars.push({act: ""})
+      }
+      this.addActivity(this.$props.currentRate)
+    },
+  }
+</script>
